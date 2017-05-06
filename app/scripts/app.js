00838ecffb05249081696022ab2239fbd5ca4674
Gleb Gorokhov <gg@verstaburo.com>
2017-05-06 22:03:14 +0300

    main popup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,11 @@
 import {VerstaSelectBox} from '../blocks/selectbox/selectbox';
+import {VerstaPopups} from '../blocks/popups/popups';
 import {advancedSearchOpened} from '../blocks/searchform/searchform';
 
 $(() => {
 
   // Финальный вызов функций
   VerstaSelectBox();
+  VerstaPopups();
   advancedSearchOpened();
 });
