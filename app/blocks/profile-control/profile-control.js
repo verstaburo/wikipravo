857b2c62e16cc94212f0cde43a1083feed85c75f
Alla Kraynova <aukra25@yandex.ru>
2017-05-11 12:51:43 +0300

    fixes

--- app/blocks/profile-control/profile-control.js
+++ app/blocks/profile-control/profile-control.js
@@ -1,11 +1,17 @@
 export function NoteButtonSwitch() {
   var button = document.querySelector(".profile-control_note");
-  button.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (button.classList.contains("active")) {
-      button.classList.remove("active");
-    } else {
-      button.classList.add("active");
-    }
-  }, false);
+  var note = document.querySelector(".profile-control__note")
+
+  if(button && note) {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (button.classList.contains("active")) {
+        button.classList.remove("active");
+        note.classList.remove("active");
+      } else {
+        button.classList.add("active");
+        note.classList.add("active");
+      }
+    }, false);
+  }
 }
