dc9c74029f296ff9ac55fb9d9724003fdcc73bb4
Alla Kraynova <aukra25@yandex.ru>
2017-05-17 10:38:16 +0300

    searchform changes

--- app/blocks/filter-controls/filter-controls.js
+++ app/blocks/filter-controls/filter-controls.js
@@ -1,25 +1,26 @@
 export function FilterOpened() {
   var mainClass = "filter-controls",
-      openBtn = mainClass + "__opened-btn",
+      openBtn = mainClass + "__open-btn",
       activationClass = "opened",
       content = mainClass + "__content",
-      icon = mainClass + "__icon",
+      icon = "button__arrow",
       btnName = "button__name",
       openedName,
       closedName;
 
-  $("." + opneBtn).click(function () {
+  $("." + openBtn).click(function () {
 
     openedName = $("." + openBtn).attr("data-opened-name");
     closedName = $("." + openBtn).attr("data-closed-name");
+    console.log(openedName + " " + closedName);
 
     if($("." + mainClass).hasClass(activationClass)) {
-      $(this).find("." + openBtn + " ." + btnName).text(openedName);
+      $("." + openBtn).find("." + btnName).text(openedName);
       $(this).find("." + icon).css({"transform" : "rotate(0)"});
-      $("." + mainClass).removeClass("opened");
+      $("." + mainClass).removeClass(activationClass);
       $("." + content).slideUp("1000");
     } else {
-      $(this).find("." + openBtn + " ." + btnName).text(closedName);
+      $("." + openBtn).find("." + btnName).text(closedName);
       $(this).find("." + icon).css({"transform" : "rotate(-180deg)"});
       $("." + mainClass).addClass(activationClass);
       $("." + content).slideDown("1000", function () {
