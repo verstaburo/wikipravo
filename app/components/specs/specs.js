13d179fb1e9051ef237c124f5ccbaa3add4cc56a
s26 <icemansbk@yandex.ru>
2017-05-30 16:16:42 +0300

    fix

--- app/components/specs/specs.js
+++ app/components/specs/specs.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
     var c_right = $('.specs__slide').find('.specs__slide-right input[type="checkbox"]');
     var c_left = $('.specs__slide').find('.specs__slide-left input[type="checkbox"]');
+    var checkbox = $('.specs__slide').find('input[type="checkbox"]');
 
-    $(c_right).change(function () {
+    $(checkbox).change(function () {
       var siblings = $(this).closest('.slide-parent').find('input[type="checkbox"]').not($(this));
-      siblings.prop('checked', true);
-      if(siblings == 'checked') { 
+      // siblings.prop('checked', true);
+      if(siblings.attr('checked') == 'checked') { 
         siblings.prop('checked', false);
+      }else{
+        siblings.prop('checked', true);
       }
-    });
-
-    $(c_left).change(function () {
-      var siblings = $(this).closest('.slide-parent').find('input[type="checkbox"]').not($(this));
-      siblings.prop('checked', false);
     });
 
     $('.specs__slide-head').click(function () {
       $(this).siblings('.specs__slide-content').fadeToggle(250);
       $(this).parents('.specs__slide').toggleClass('specs__slide_active');
+    }).children().find('.checkbox').click(function (e) {
+      e.stopPropagation();
     });
 });
