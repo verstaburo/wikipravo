7786281c95e85ed63af2fc0b6a1c2a33eb58cca5
Alla Kraynova <aukra25@yandex.ru>
2017-06-01 12:38:23 +0300

    registration page specs counter

--- app/components/specs/specs.js
+++ app/components/specs/specs.js
@@ -1,15 +1,43 @@
-$(document).ready(function () {
+export function SpecsCheckboxActions() {
+
+  $(document).ready(function () {
     var checkbox = $('.specs__slide').find('input[type="checkbox"]');
 
     $(checkbox).change(function () {
-      var siblings = $(this).closest('.slide-parent').find('input[type="checkbox"]').not($(this));
-      siblings.prop('checked', $(this).prop("checked"))
+      var siblings = $(this).parents('.slide-parent').find('input[type="checkbox"]').not($(this));
+      siblings.prop('checked', $(this).prop("checked")).onchange();
     });
 
-    $('.specs__slide-head').click(function () {
-      $(this).siblings('.specs__slide-content').fadeToggle(250);
+    $('.specs__arrow').click(function () {
+      $(this).parents('.specs__slide').find('.specs__slide-content').fadeToggle(250);
       $(this).parents('.specs__slide').toggleClass('specs__slide_active');
-    }).children().find('.checkbox').click(function (e) {
-      e.stopPropagation();
     });
-});
+  });
+
+  function CheckboxCount (slideEl) {
+    var checboxArray = [];
+    var checked = [];
+
+    checboxArray = $(slideEl).find('.specs__slide-content').find('input[type="checkbox"]');
+
+    for(var i = 0; i < checboxArray.length; i++) {
+      if(checboxArray[i].checked) {
+        checked.push(checboxArray[i].checked);
+      }
+    }
+
+    return checked.length/2;
+  }
+
+  setInterval(function () {
+    var slides = $('.specs__slide');
+    for(var k = 0; k < slides.length; k++) {
+      var count = CheckboxCount(slides[k]);
+      if(count > 0) {
+        $(slides[k]).find('.specs__count').text('(' + count + ')');
+      } else {
+        $(slides[k]).find('.specs__count').text('');
+      }
+    }
+  }, 500);
+};
