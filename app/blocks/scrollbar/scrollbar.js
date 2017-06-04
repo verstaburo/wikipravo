6ea1dd9201bfb5fc3c1b0777e14248e7799f38cd
Alla Kraynova <aukra25@yandex.ru>
2017-06-04 22:25:43 +0300

    fixes

--- app/blocks/scrollbar/scrollbar.js
+++ app/blocks/scrollbar/scrollbar.js
@@ -1,6 +1,7 @@
 export function ScrollBar() {
   $(document).ready(function(){
     $('.selectbox_scrollable .selectbox__list').scrollbar();
+    $('.sidebar').scrollbar();
     $(document).on("click", ".scroll-bar", function(e) {
       e.stopPropagation();
     });
